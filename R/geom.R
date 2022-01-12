cca38a6fdb94ca91fee500e7163f2a24ee97d273
tpemartin <tpemartin@gmail.com>
2022-01-12 14:02:54 +0800

    update

--- R/geom.R
+++ R/geom.R
@@ -44,9 +44,11 @@ geom_overlay <- function(geom,
 #' @param map_id A character showing the name of the map_id in data input. The data[[map_id]] will be used to join with sf_taiwan_simplied$台灣本島$xxx's map_id column where xxx is determined by type input value.
 #' @param ... other parameters passed to geom_sf
 #' @param type either "縣市" or "鄉鎮區"
+#' @param need_background default is TRUE. If FALSE, just return the choropleth layer.
 #' @param background.fill the fille of background Taiwan map
 #' @param background.color the color of background Taiwan map
 #' @param background.size the size of boundary of background map
+#' @param merge_type default="joint". "join" means sf inner_join data, "map" means sf left_join data, "data" means sf right_join data
 #'
 #' @return
 #' @export
@@ -61,10 +63,12 @@ geom_overlay <- function(geom,
 #'   )
 geom_sf_taiwan <- function(data, map_id, ...,
   type=c("縣市", "鄉鎮區"),
+  cast2multipolygon = T,
+  merge_type="joint",
+  need_background = T,
   background.fill= "#c8c5be",
   background.color= "#c8c5be",
-  background.size= 0,
-  cast2multipolygon = T
+  background.size= 0
 ) {
 
   type = match.arg(type)
@@ -74,23 +78,42 @@ geom_sf_taiwan <- function(data, map_id, ...,
     .sf <- sf::st_cast(.sf, "MULTIPOLYGON")
   }
 
+  merge_fun <-
+  switch(
+    merge_type,
+    "joint"={dplyr::inner_join},
+    "data"={dplyr::right_join},
+    "map"={dplyr::left_join}
+  )
   which2pick <- which(.sf$map_id %in% data[[map_id]])
   .sf[which2pick, ] |>
-    dplyr::left_join(
+    merge_fun(
       data,
       by=c("map_id"=map_id)
     ) -> .sf_choropleth
 
-  list(
-    geom_sf(
-      data=.sf,
-      fill=background.fill,
-      color=background.color,
-      size=background.size
-    ),
-    geom_sf(
-      data=.sf_choropleth,
-      ...
+  if(isFALSE(need_background)){
+    return(
+      geom_sf(
+        data=.sf_choropleth,
+        ...
+      )
     )
-  )
+  } else {
+    return(
+      list(
+        geom_sf(
+          data=.sf,
+          fill=background.fill,
+          color=background.color,
+          size=background.size
+        ),
+        geom_sf(
+          data=.sf_choropleth,
+          ...
+        )
+      )
+    )
+  }
+
 }
