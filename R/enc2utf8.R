7fcec99a483bc2863520bb60f92a15a5df7ed053
tpemartin <tpemartin@gmail.com>
2022-01-16 10:23:45 +0800

    Update enc2utf8.R

--- R/enc2utf8.R
+++ R/enc2utf8.R
@@ -7,7 +7,8 @@ enc2utf8_all <- function(.sf){
 }
 get_sf_taiwan4windows <- function(){
   econDV2::sf_taiwan_simplified -> sf_taiwan_simplified
-  enc2utf8(names(sf_taiwan_simplified)) -> names(sf_taiwan_simplified)
+  names(sf_taiwan_simplified) <-
+    c("縣市", "鄉鎮區", "台灣本島")
   sf_taiwan_simplified[c(1,2)] |>
     purrr::map(enc2utf8_all) -> sf_taiwan_simplified[c(1,2)]
 
