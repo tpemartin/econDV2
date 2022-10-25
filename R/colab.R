dc101c2349b6bf8ad40e5ebe7b4c92886c4a026d
tpemartin <tpemartin@gmail.com>
2022-10-25 13:35:52 +0800

    Update colab.R

--- R/colab.R
+++ R/colab.R
@@ -1,8 +1,6 @@
 serveJupyterNotebook = function(){
   rstudioapi::terminalExecute(
-    "jupyter notebook \
-  --NotebookApp.allow_origin='https://colab.research.google.com' \
-  --port=4321 \
-  --NotebookApp.port_retries=0"
+    "jupyter serverextension enable --py jupyter_http_over_ws
+    jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888  --NotebookApp.port_retries=0"
   )
 }
