7051e598fbf5281172c01e05973c19689fde9200
Martin老師 <tpemartin@gmail.com>
2022-11-09 15:21:59 +0800

    update version

--- R/colab.R
+++ R/colab.R
@@ -1,6 +1,5 @@
 serveJupyterNotebook = function(){
-  rstudioapi::terminalExecute(
-    "jupyter serverextension enable --py jupyter_http_over_ws
-    jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888  --NotebookApp.port_retries=0"
-  )
+  termId = rstudioapi::terminalCreate(shellType="default")
+  rstudioapi::terminalSend(id=termId, "jupyter serverextension enable --py jupyter_http_over_ws
+    jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888  --NotebookApp.port_retries=0\n")
 }
